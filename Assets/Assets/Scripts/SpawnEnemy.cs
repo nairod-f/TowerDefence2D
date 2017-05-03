6876041fc1f8aa220938eea74d39f5a876065f4d
nairod-f <Dorian.fenech.22@gmail.com>
2017-05-04 00:00:50 +0200

    GUI Elements
    Trying to geth the GUI Elements working together, although i cannot seem
    to get the "new wave" text to work properly. Still have to make health
    decrease with each bug. And i managed to make the wave counter increse
    as well as the gold is working well

--- Assets/Assets/Scripts/SpawnEnemy.cs
+++ Assets/Assets/Scripts/SpawnEnemy.cs
@@ -5,13 +5,69 @@ using UnityEngine;
 public class SpawnEnemy : MonoBehaviour {
     public GameObject[] waypoints;
     public GameObject testEnemyPrefab;
+
+    public Wave[] waves;
+    public int timeBetweenWaves = 5;
+
+    private GameManagerBehaviour gameManager;
+
+    private float lastSpawnTime;
+    private int enemiesSpawned = 0;
+
+
+    [System.Serializable]
+    //This class is Serializable, which means you can change the values in the Inspector.
+    public class Wave
+    {
+        public GameObject enemyPrefab;
+        public float spawnInterval = 2;
+        public int maxEnemies = 20;
+    }
+
     // Use this for initialization
     void Start () {
-        Instantiate(testEnemyPrefab).GetComponent<MoveEnemy>().waypoints = waypoints;
+        lastSpawnTime = Time.time;
+        gameManager =
+            GameObject.Find("GameManager").GetComponent<GameManagerBehaviour>();
     }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        // index of the current wave, and check if it’s the last one.
+        int currentWave = gameManager.Wave;
+        if (currentWave < waves.Length)
+        {
+            //calculate how much time passed since the last enemy spawn and whether it’s time to spawn an enemy
+            float timeInterval = Time.time - lastSpawnTime;
+            float spawnInterval = waves[currentWave].spawnInterval;
+            if (((enemiesSpawned == 0 && timeInterval > timeBetweenWaves) ||
+                 timeInterval > spawnInterval) &&
+                enemiesSpawned < waves[currentWave].maxEnemies)
+            {
+                //spawn an enemy by instantiating a copy of enemyPrefab. You also increase the enemiesSpawned count. 
+                lastSpawnTime = Time.time;
+                GameObject newEnemy = (GameObject)
+                    Instantiate(waves[currentWave].enemyPrefab);
+                newEnemy.GetComponent<MoveEnemy>().waypoints = waypoints;
+                enemiesSpawned++;
+            }
+            // check the number of enemies on screen. 
+            if (enemiesSpawned == waves[currentWave].maxEnemies &&
+                GameObject.FindGameObjectWithTag("Enemy") == null)
+            {
+                gameManager.Wave++;
+                gameManager.Gold = Mathf.RoundToInt(gameManager.Gold * 1.1f);
+                enemiesSpawned = 0;
+                lastSpawnTime = Time.time;
+                //if there are none (last enemy)-> spawn next wave and yield gold
+            }
+            //beating the last wave this runs the game won animation. 
+        }
+        else
+        {
+            gameManager.gameOver = true;
+            GameObject gameOverText = GameObject.FindGameObjectWithTag("GameWon");
+            gameOverText.GetComponent<Animator>().SetBool("gameOver", true);
+        }
+    }
 }
